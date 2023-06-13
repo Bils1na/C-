09d46ef7b30ccc441e3319208d83bb4746f0a927
bilsina <bils1n@icloud.com>
2023-06-13 17:39:37 +0300

    added lecture2

---
+++ lecture2/example10/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 51, 82, 53, 64, 25, 46, 27, 18, 27 };
+
+int n = array.Length;
+int find = 27;
+
+int index =  0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
