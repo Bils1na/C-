9297e51325f3df3ed0f86a33dbd3b997cd66fb3e
bilsina <bils1n@icloud.com>
2023-06-23 20:40:40 +0300

    added homework4

---
+++ homework4/task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double Degree(int number, int power)
+{
+    double result = Math.Pow(number, power);
+
+    return result;
+}
+
+System.Console.Write("Enter number >> ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter power >> ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Degree(number, power));
---
+++ homework4/task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и выводит 
+// их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+int[] DefineLengthArray()
+{
+    int array_length = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int [array_length];
+    
+    return array;
+}
+
+System.Console.Write("Enter length for array >> ");
+int[] array = DefineLengthArray();
+System.Console.Write(string.Join(", ", FillArray(array)));
