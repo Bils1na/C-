35a20aaa61cd7ca4089d956a9ed58560246caf74
bilsina <bils1n@icloud.com>
2023-07-04 19:28:57 +0300

    added seminar7

---
+++ seminar7/task49/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса 
+// чётные, и замените эти элементы на их квадраты.
+
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть
+// вот так:
+// 1^2 4 49 2
+// 5 81 2 9
+// 64 4 4 4
+
+void FillMatrix(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SquareNumberForEvenIndex(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+}
+
+
+System.Console.Write("Enter number rows >> ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter number columns >> ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+
+System.Console.WriteLine("Simple matrix >> ");
+FillMatrix(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine("Squares matrix >> ");
+SquareNumberForEvenIndex(matrix);
+PrintMatrix(matrix);
---
+++ seminar7/task51/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса 
+// чётные, и замените эти элементы на их квадраты.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void FillMatrix(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SumElementsDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+
+    return sum;
+}
+
+
+System.Console.Write("Enter number rows >> ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter number columns >> ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+
+System.Console.WriteLine("Simple matrix >> ");
+FillMatrix(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine($"Sum of elements of main diagonal = {SumElementsDiagonal(matrix)}");
