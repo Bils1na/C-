25521963c81a824020b7e68a9316f09f321d51a0
bilsina <bils1n@icloud.com>
2023-07-04 18:44:05 +0300

    added homework6

---
+++ homework6/task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с 
+// клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return arr;
+}
+
+int PositiveNumbers(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+System.Console.Write("Enter number elements of array >> ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(size);
+
+System.Console.WriteLine($"[{string.Join(", ", array)}] -> {PositiveNumbers(array)}");
