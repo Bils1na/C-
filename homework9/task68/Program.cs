98f2b1be5d3e2d835410aa82f9ad49dd788eb02e
bilsina <bils1n@icloud.com>
2023-07-13 20:02:25 +0300

    added lecture5

--- homework9/task68/Program.cs
+++ homework9/task68/Program.cs
@@ -6,18 +6,18 @@
 
 int AkkermanFunction(int m, int n)
 {
-    if (m == 0) 
+    if (m == 0)
     {
         return n + 1;
     }
-    
-    if (m != 0 && n == 0) 
+
+    if (m != 0 && n == 0)
     {
         return AkkermanFunction(m - 1, 1);
     }
-    if ( m > 0 && n > 0)
-    { 
-        return AkkermanFunction(m - 1, 
+    if (m > 0 && n > 0)
+    {
+        return AkkermanFunction(m - 1,
         AkkermanFunction(m, n - 1));
     }
     return AkkermanFunction(m, n);
---
+++ lecture5/example14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+int a = 12;
+System.Console.WriteLine(a.GetType().Name);
+
+var b = 12;
+System.Console.WriteLine(b.GetType().Name);
+
+var data = new int[] { 1, 2, 3, 4, 5 }
+        .Where(e => e > 0)
+        .Select(e => new { q = e, w = e + 1 });
+System.Console.WriteLine(data.GetType().Name);
